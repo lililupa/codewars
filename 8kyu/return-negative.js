83f567f1bdec3b32ada419d8d30b5c67558409e9
Aryna Koushyk <aryna.koushyk@gmail.com>
2025-10-30 02:03:38 +0300

    complete task

---
+++ 8kyu/return-negative.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else if (num < 0) {
+    return num;
+  } else {
+    return 0;
+  }
+}
