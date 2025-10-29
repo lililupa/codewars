62df2225204619e0d5873e1568383c055e38b2d2
Aryna Koushyk <aryna.koushyk@gmail.com>
2025-10-30 01:50:23 +0300

    complete task

---
+++ 8kyu/multiply.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//   a * b
+// }
+
+//SOLUTION:
+function multiply(a, b) {
+  let c = a * b;
+  return c;
+}
+let mul = multiply(2, 2);
+console.log(mul);
